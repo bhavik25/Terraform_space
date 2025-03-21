114b0cc97e43eb9008349246226d4a00664b913d
bhavik25 <bhavik25@live.com>
2025-03-21 14:24:38 +0000

    Changes

--- versions.tf
+++ versions.tf
@@ -5,14 +5,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 0.15.0"
 }
